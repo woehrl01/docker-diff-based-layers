f7e1ea67863d42d243f20b71c4489489759e7be9
Lukas Wöhrl <lukas.woehrl@plentymarkets.com>
2023-04-15 23:56:56 +0200

    fix: ignore timestamp and don't print folder creations

--- create-changelog-to-go-from-old-to-new.sh
+++ create-changelog-to-go-from-old-to-new.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get install -y rsync
 
 # Use rsync to figure out what changes are necessary to go from old image contents to new
 echo "List of changes necessary to go from old image contents to new: "
-rsync -a -x --human-readable --delete-after --checksum --dry-run --itemize-changes --exclude .docker-image-diff "$RESTRICT_DIFF_TO_PATH/" "rsync://rsync@old/root$RESTRICT_DIFF_TO_PATH/" | tee $OUT/changes.rsync.log
+rsync -a -x --no-times --human-readable --delete-after --checksum --dry-run --itemize-changes --exclude .docker-image-diff "$RESTRICT_DIFF_TO_PATH/" "rsync://rsync@old/root$RESTRICT_DIFF_TO_PATH/" | tee $OUT/changes.rsync.log
 
 # Add files to add to a tar archive
 cat $OUT/changes.rsync.log | grep  -E '^<f|^cL|^hf' | while read -a cols; do echo "$RESTRICT_DIFF_TO_PATH/"${cols[@]:1}; done > $OUT/files-to-add_2.list
@@ -28,7 +28,7 @@ echo "Number of files to remove: "
 wc $OUT/files-to-remove.list
 echo
 echo "Changes not accounted for: "
-cat $OUT/changes.rsync.log | grep -v -E '^<f|^cL|^hf'  | grep -v '^*deleting'
+cat $OUT/changes.rsync.log | grep -v -E '^<f|^cL|^hf|^cd'  | grep -v '^*deleting' || [[ $? == 1 ]]
 echo
 echo "Press CTRL-C to continue"
 
